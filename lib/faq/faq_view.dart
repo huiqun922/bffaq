2582cc4c6e917f2cf40123f628a0eee5c4fe4d71
Hay <498427002@qq.com>
2023-08-15 20:19:32 +0800

    faq

--- lib/faq/faq_view.dart
+++ lib/faq/faq_view.dart
@@ -8,13 +8,13 @@ import 'package:flutter/material.dart';
 
 import 'flutter_faq.dart';
 
-class FAQView extends StatelessWidget{
+class FAQView extends StatelessWidget {
   const FAQView({super.key});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    List<String>? products = ['BF-5/BF-6','BF-20','BF-30/BF-40'];
+    List<String>? products = ['BF-5/BF-6', 'BF-20', 'BF-30/BF-40'];
 
     return Scaffold(
       // appBar: BFAppBar.back(titleStr: "FAQ",backAction: (){
@@ -23,19 +23,22 @@ class FAQView extends StatelessWidget{
       body: CardView(
         padding: EdgeInsets.symmetric(horizontal: 8.scale),
         child: ListView.builder(
-          itemCount: products!.length,
-            itemBuilder: (ctx,index){
-            return ListViewCell(title:products[index],click: (){
-              //Route route = MaterialPageRoute(builder: (ctx)=>FAQViewDetail(type: index,));
-              Navigator.pushNamed(context, '/faq/detail',arguments: index);
-            },);
+            itemCount: products!.length,
+            itemBuilder: (ctx, index) {
+              return ListViewCell(
+                title: products[index],
+                click: () {
+                  //Route route = MaterialPageRoute(builder: (ctx)=>FAQViewDetail(type: index,));
+                  Navigator.pushNamed(context, '/faq/detail', arguments: index);
+                },
+              );
             }),
       ),
     );
   }
 }
 
-class FAQViewDetail extends StatelessWidget{
+class FAQViewDetail extends StatelessWidget {
   FAQViewDetail({super.key});
   int type = 0;
   List faqs = [];
@@ -55,22 +58,22 @@ class FAQViewDetail extends StatelessWidget{
         padding: EdgeInsets.symmetric(horizontal: 8.scale),
         child: ListView.builder(
             itemCount: faqs!.length,
-            itemBuilder: (ctx,index){
+            itemBuilder: (ctx, index) {
               return FAQ(
                 question: faqs[index].q,
                 answer: faqs[index].a,
-                ansPadding: EdgeInsets.symmetric(vertical:10.scale,horizontal: 20),
+                ansPadding:
+                    EdgeInsets.symmetric(vertical: 10.scale, horizontal: 20),
                 queStyle: TextUtils.mediumDartText(fontSize: 16),
                 ansStyle: TextUtils.regularGrayText(fontSize: 14),
-                queDecoration: const BoxDecoration(
-                    color: textWhiteColor),
-                ansDecoration: const BoxDecoration(
-                    color: bgLightGrayColor),
+                queDecoration: const BoxDecoration(color: textWhiteColor),
+                ansDecoration: const BoxDecoration(color: bgLightGrayColor),
               );
             }),
       ),
     );
   }
+
   /*
   BF-20:
 Q:What is the PIN code? Why can’t connect to the APP?
@@ -87,8 +90,8 @@ Q: Why no data is displayed after power on?
 A: If the probe is not plugged into the socket after powering on, the device will not display temperature and auto power off after 3 minutes.
 
    */
-  void setupFAQ(){
-    if(type==0 || type==1){
+  void setupFAQ() {
+    if (type == 0 || type == 1) {
       faqs = [
         FAQModel(
             'Q: Why our App need to ask for your location (GPS)? It is safe?',
@@ -108,11 +111,13 @@ A: If the probe is not plugged into the socket after powering on, the device wil
             'A: If the probe is not plugged into the socket after powering on, the device will not display temperature and auto power off after 3 minutes.'),
       ];
     }
-    if(type==1){
-      faqs.add(FAQModel('Q:How to charge?',
-          'A:\n1.Can charge devices, including computers and mobile power, through ordinary chargers.\n2.Cannot use fast charger with excessive output power.'),);
+    if (type == 1) {
+      faqs.add(
+        FAQModel('Q:How to charge?',
+            'A:\n1.Can charge devices, including computers and mobile power, through ordinary chargers.\n2.Cannot use fast charger with excessive output power.'),
+      );
     }
-    if(type==2){
+    if (type == 2) {
       faqs = [
         FAQModel(
             'Q: Why our App need to ask for your location (GPS)? It is safe?',
@@ -126,14 +131,13 @@ A: If the probe is not plugged into the socket after powering on, the device wil
         FAQModel(
             'Q: If I go out of range, how do I reconnect my smart device to the thermometer?',
             'A:The signal will reconnect automatically with your smart device once your smart device is back within range.'),
-        FAQModel(
-            'Q: Can I use the thermometer in the microwave ?',
+        FAQModel('Q: Can I use the thermometer in the microwave ?',
             'A:No , you can\'t use your thermometer in the microwave.'),
         FAQModel('Q: How should I disarm the booster\'s alarm sound?',
-    'A:\n(1) When the booster reaches the target temperature, the booster will sound an alarm, and the alarm can be stopped by pressing the power button.\n(2) When the booster and the probe are disconnected,the booster will sound an alarm and the alarm can be stopped by pressing the power button twice.\n(3) When the booster temperature is out of temperature range to avoid probe damage, the booster will keep sounding the alarm until the temperature is in the normal range (32℉-212℉ /0℃-100℃).'
-        ),
-        FAQModel('Q: What should I do if no data is displayed after powering on?',
-        'A: If the temperature is not displayed upon powering on, it may be due to the probe not being plugged into the socket properly. In such cases, you can reinsert the probe into the socket and remove it again to ensure proper display of the temperature.'),
+            'A:\n(1) When the booster reaches the target temperature, the booster will sound an alarm, and the alarm can be stopped by pressing the power button.\n(2) When the booster and the probe are disconnected,the booster will sound an alarm and the alarm can be stopped by pressing the power button twice.\n(3) When the booster temperature is out of temperature range to avoid probe damage, the booster will keep sounding the alarm until the temperature is in the normal range (32℉-212℉ /0℃-100℃).'),
+        FAQModel(
+            'Q: What should I do if no data is displayed after powering on?',
+            'A: If the temperature is not displayed upon powering on, it may be due to the probe not being plugged into the socket properly. In such cases, you can reinsert the probe into the socket and remove it again to ensure proper display of the temperature.'),
         FAQModel('Q:Why can\'t it charge?',
             'A: Please check if the charging spring of the probe slot is loose, if it is loose, please contact us!'),
         FAQModel('Q:What should I do if I get a white screen?',
@@ -142,14 +146,20 @@ A: If the probe is not plugged into the socket after powering on, the device wil
             'A: \n1.Connect in an outdoor open area with a wireless range of up to 200 ft (60 m) from the booster to the phone and up to 32 ft (10 ms) from the probe to the booster.\n2. The actual Bluetooth range also relates to the cell phone you are using and the cell phone\'s Bluetooth version.\n(Actual wireless range may vary on a case-by-case basis).'),
         FAQModel('Q:How to place the probes correctly on your BF-40?',
             'A: Black probes on the left, white probes on the right'),
-
+        FAQModel('Q：How can I pair my smart devices withthe booster ?',
+            'A：\n1 ) Enable Bluetooth on your smart device under the Settings section.\n2 ) Turn on GPS on your Android smart device.\n3 ) Remove the probe from the booster.\n4 ) Launch the BF Home APP in your smart device and press Add Device , A list of BF-40/30 devices will appear and select BF-40/30 from the list.then select the "Grill BT The APP will jump to the device list page which shows the real time temperature measured by the probe. Now your BF-40/30 is ready for use . You don\'t need to pair them again for future uses.'),
+        FAQModel(
+            'Q: Why does the supercharger sound an alarm and how do I disarm it?',
+            'A:\n(1) When the probe reaches the target temperature, the booster will sound an alarm and can be disarmed by pressing the power button.\n(2) When the booster is disconnected from the probe, the booster will sound an alarm, press the power button twice to disarm the alarm.\n(3) When the probe is out of temperature range, the booster will sound an alarm until the temperature drops.'),
+        FAQModel('Q:How is a cooking temperature graph created and stored?',
+            'A:After you select the meat type and doneness and press Start "Start Cooking" button, the APP will record the internal temperature of the meat and create a temperature graph that you can save at any time.When an alarm is issued when the set temperature is reached, the temperature graph will be automatically saved to the history alarm record.')
       ];
     }
   }
 }
 
-class FAQModel{
+class FAQModel {
   String q;
   String a;
-  FAQModel(this.q,this.a);
+  FAQModel(this.q, this.a);
 }
