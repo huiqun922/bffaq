5b37e09159db485845ecf64f6c27f31c0ed0ed79
Hay Huang <498427002@qq.com>
2026-08-09 11:19:17 +0800

    new faq

--- lib/faq/faq_view.dart
+++ lib/faq/faq_view.dart
@@ -1,12 +1,7 @@
 import 'dart:convert';
-import 'dart:html';
 
 import 'package:bfweb/utils/global.dart';
-import 'package:bfweb/utils/size_fit.dart';
-import 'package:bfweb/utils/text_utils.dart';
-import 'package:bfweb/utils/ui/app_bar.dart';
 import 'package:bfweb/utils/ui/card.dart';
-import 'package:bfweb/utils/ui/listview_cell.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'flutter_faq.dart';
@@ -16,102 +11,57 @@ class FAQView extends StatefulWidget {
   const FAQView(this.locale, {super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _FAQViewState();
-  }
+  State<FAQView> createState() => _FAQViewState();
 }
 
 class _FAQViewState extends State<FAQView> {
-  List<String> products = [];
-  Map<String, dynamic> jsonMap = {};
-
+  List<FAQModel> faqs = [];
 
   Future<void> loadJsonData() async {
-
-    List<String>? locals = ['en','de','it','nl','fr','es'];
-    String local = widget.locale;
-    if(locals.contains(local) == false){
-      local = 'en';
+    const supportedLocales = ['en', 'zh', 'de', 'it', 'nl', 'fr', 'es'];
+    var locale = widget.locale;
+    if (!supportedLocales.contains(locale)) {
+      locale = 'en';
     }
-    String jsonData =
-    await rootBundle.loadString('assets/json/faq_$local.json'); // 替换成你的JSON文件路径
-    jsonMap = jsonDecode(jsonData);
+
+    var jsonData = await rootBundle.loadString('assets/json/faq_$locale.json');
+    final List<dynamic> jsonList = jsonDecode(jsonData);
     setState(() {
-      products = List.from(jsonMap.keys);
+      faqs = jsonList
+          .map((item) => FAQModel(item['q'] as String, item['a'] as String))
+          .toList();
     });
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     loadJsonData();
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       body: CardView(
         padding: EdgeInsets.symmetric(horizontal: 8.scale),
         child: ListView.builder(
-            itemCount: products!.length,
-            itemBuilder: (ctx, index) {
-              return ListViewCell(
-                title: products[index],
-                click: () {
-                  Route route = MaterialPageRoute(
-                      builder: (ctx) => FAQViewDetail(
-                        map: jsonMap[products[index]],
-                      ));
-                  Navigator.push(context, route);
-                },
-              );
-            }),
+          itemCount: faqs.length,
+          itemBuilder: (ctx, index) {
+            return FAQ(
+              question: faqs[index].q,
+              answer: faqs[index].a,
+              ansPadding:
+                  EdgeInsets.symmetric(vertical: 10.scale, horizontal: 20),
+              queStyle: TextUtils.mediumDartText(fontSize: 16),
+              ansStyle: TextUtils.regularGrayText(fontSize: 14),
+              queDecoration: const BoxDecoration(color: textWhiteColor),
+              ansDecoration: const BoxDecoration(color: bgLightGrayColor),
+            );
+          },
+        ),
       ),
     );
   }
-}
-
-
-
-class FAQViewDetail extends StatelessWidget {
-  final List<dynamic> map;
-  FAQViewDetail({super.key, this.map = const []});
-
-  List faqs = [];
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    setupFAQ();
-    return Scaffold(
-      body: CardView(
-        padding: EdgeInsets.symmetric(horizontal: 8.scale),
-        child: ListView.builder(
-            itemCount: faqs!.length,
-            itemBuilder: (ctx, index) {
-              return FAQ(
-                question: faqs[index].q,
-                answer: faqs[index].a,
-                ansPadding:
-                EdgeInsets.symmetric(vertical: 10.scale, horizontal: 20),
-                queStyle: TextUtils.mediumDartText(fontSize: 16),
-                ansStyle: TextUtils.regularGrayText(fontSize: 14),
-                queDecoration: const BoxDecoration(color: textWhiteColor),
-                ansDecoration: const BoxDecoration(color: bgLightGrayColor),
-              );
-            }),
-      ),
-    );
-  }
-
-  void setupFAQ() {
-    for (var element in map) {
-      String key = element.keys.first;
-      faqs.add(FAQModel(key,element[key]));
-    }}
 }
 
 class FAQModel {
